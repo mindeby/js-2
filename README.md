eddf3ecf9b03d3610ffeff4e912f5d900cd95dd5
mindeby <debora.marisa.ribeiro@gmail.com>
2019-11-13 12:01:38 +0100

    added read me text

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
  List Pagination and Filtering
 
 
-This project will take a list of data and divide it into pages for better readability.
+With the use of javascript we'll create 2 functions. One whose purpose is to
+create pagination links according to how many items (from any given list) we want to show on each page and
+another function whose purpose is to display the items that correspond to the pagination
+link that was clicked.
+
+I followed all the instructions given and actively didn't make any changes to any of the files except the script.js file.
+
+This project was great practice with Traversing the DOM and also gave me a great opportunity to practice debugging
+using Chrome's Dev Tools.
+
+I've attempted to earn the 'Exceeds Expectations' grade by adding a search feature that lets you filter the list to match your searched criteria.
--- js/script.js
+++ js/script.js
@@ -4,27 +4,33 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 
-let studentNames = document.querySelectorAll('H3');
-
+// I created only two global variables for my functions
 let studentList = document.querySelectorAll('li'); //each student is an li in the index.html
 
 let itemsPerPage = 10;
 
 
+
+//Just capitalizing the names of the students
 for (i = 0; i < studentNames.length; i++) {
- studentNames[i].style.textTransform ="capitalize"; //Just capitalizing the names of the students
+ studentNames[i].style.textTransform ="capitalize";
 }
 
+let studentNames = document.querySelectorAll('H3');
+//
 
-const showPage = (list, page) => { //function to show the page we want
 
-  let startIndex = (page * itemsPerPage) - itemsPerPage;
-  let endIndex = page * itemsPerPage -1;
+
+//function to show the page we want according to each starting and ending index
+const showPage = (list, page) => { //takes as arguments any given list and which page we want to select
+
+  let startIndex = (page * itemsPerPage) - itemsPerPage; //Ex: For Page 2 * 10 items per page = 20 - 10 items per Page means the starting index is 10
+  let endIndex = page * itemsPerPage -1; //Ex: For Page 2 * 10 items per page = 20 - 1 items per Page means the ending index is 19
 
   for (i = 0; i < list.length; i++) {
-    list[i].style.display = 'none';
+    list[i].style.display = 'none'; //for as long as the list goes hide all the elements
     if (i >= startIndex && i <= endIndex ) {
-      list[i].style.display = 'block';
+      list[i].style.display = 'block'; //and only show back the ones in the range of the starting and ending indexes we want
     }
   }
 };
@@ -33,10 +39,10 @@ const showPage = (list, page) => { //function to show the page we want
 // add pagination links
 
 const addPaginationLinks = (list) => {
-  removeElementsByClass('pagination') //remove old elements every time you start running the function
-  let numberOfButtons = Math.ceil(list.length / itemsPerPage);
-  let htmlDiv = document.getElementsByClassName('page')[0]; //get the div with the class of .page to add buttons to the bottom
-  let newDiv = document.createElement('DIV');
+  removeElementsByClass('pagination') //remove old elements every time you start running the function so that we don't create endless elements
+  let numberOfButtons = Math.ceil(list.length / itemsPerPage); //We need to round up the number of pages/buttons even if we don't get a full page
+  let htmlDiv = document.getElementsByClassName('page')[0]; //get the div with the class of .page to add child elements to (in this case the buttons for the pagination links)
+  let newDiv = document.createElement('DIV'); //creating and nesting elements to hold each pagination link in the webpage
   let newUl = document.createElement('UL');
   newDiv.classList.add("pagination")
   htmlDiv.appendChild(newDiv)
@@ -50,8 +56,8 @@ const addPaginationLinks = (list) => {
     newUl.appendChild(li)
     link.innerText = `${i+1}` //add numbers to a tags
 
-    if ( i == 0) {
-    link.classList.add("active")
+    if ( i == 0) { //if it is the first/current page
+    link.classList.add("active") //always starts with an "active" state, it's the page we're on
     }
 
   }
@@ -61,11 +67,11 @@ const addPaginationLinks = (list) => {
   for (i = 0; i < links.length; i++) {
     links[i].addEventListener("click", function(){
       for (i = 0; i < links.length; i++) {
-       links[i].classList.remove('active')
+       links[i].classList.remove('active') //remove the active state for all the pagination links when any of them is clicked
       }
 
-      event.target.classList.add('active')
-      showPage(studentList, event.target.innerText) //call the function
+      event.target.classList.add('active') //but add it back to the selected pagination link
+      showPage(studentList, event.target.innerText) //call the function taking the list items and setting the number of the page according to the text on the buttons
 
     });
   }
@@ -76,7 +82,7 @@ showPage(studentList, 1) //call the function
 addPaginationLinks(studentList) //call the function
 
 
-function removeElementsByClass(className){ //function to remove the old pagination links when the functions are called
+function removeElementsByClass(className){ //function to remove the old pagination links when the functions are called as a "restart"
   var elements = document.getElementsByClassName(className);
   while(elements.length > 0){
       elements[0].parentNode.removeChild(elements[0]);
@@ -86,39 +92,45 @@ function removeElementsByClass(className){ //function to remove the old paginati
 //For the Extra Credit
 
 // Add search component
-let headerDiv = document.getElementsByClassName('page-header')[0]; //get the div with the class of .page to add buttons to the bottom
-let searchBar = document.createElement('DIV')
+let headerDiv = document.getElementsByClassName('page-header')[0]; //get the div with the class of .page to add child elements to (in this case the buttons for the pagination links)
+let searchBar = document.createElement('DIV') //Creating new elements to the page
 let searchBox = document.createElement('INPUT')
 let searchButton = document.createElement('BUTTON')
+
+//Styling of the elements created
 searchBar.classList.add("student-search")
 stylingElements(searchButton)
 searchButton.style.backgroundColor="#4ba6c3"; //aditional button styling
 searchButton.style.color="#fff"; //aditional button styling
 stylingElements(searchBox)
 searchBox.style.marginRight="5px"; //aditional search box styling
-headerDiv.appendChild(searchBar)
+//
+
+
+headerDiv.appendChild(searchBar) //Nesting created elements
 searchBar.appendChild(searchBox)
 searchBar.appendChild(searchButton)
 searchBox.setAttribute("placeholder", "Search for students...")
 searchButton.innerText = "Search"
 
-const findMatches = () => {
-  let search = searchBox.value.toLowerCase();
-  let results =[]
-  removeElementsByClass('no_results') //remove old paragraphs
+const findMatches = () => { //function to find new matches on a certain event
+  let search = searchBox.value.toLowerCase(); //search works even if input is in all caps
+  let results =[] //create an array to store the matches
+  removeElementsByClass('no_results') //remove old paragraphs we created every time we run the function
+
   for (i = 0; i < studentList.length; i++) {
-    studentList[i].style.display = 'none';
+    studentList[i].style.display = 'none'; //hide every element on the list
     if (studentList[i].innerText.indexOf(search) !== -1) {
-      results.push(studentList[i])
+      results.push(studentList[i]) //unless they match the search, in that case push the item to the results array
     }
   }
-  showPage(results, 1)
-  addPaginationLinks(results)
+  showPage(results, 1) //call function
+  addPaginationLinks(results) //call function
 
   if (results.length == 0) { //if there are no matches
-    removeElementsByClass('no_results') //remove old paragraphs
-    let parent = document.getElementsByClassName('page-header')[0]; //create a new paragraph for the message
-    let newP = document.createElement('P');
+    removeElementsByClass('no_results') //remove old paragraphs  we created every time we run the function
+    let parent = document.getElementsByClassName('page-header')[0]; //get the parent div
+    let newP = document.createElement('P'); //create a new paragraph for the message
     newP.classList.add("no_results")
     parent.appendChild(newP)
     newP.innerText = "No results found"
@@ -136,7 +148,7 @@ searchBox.addEventListener("keyup", function(){ //search on keyup
 });
 
 
-function stylingElements(element){ //function to style the button and search bar
+function stylingElements(element){ //function to style the button and search box
   element.style.borderRadius="5px";
   element.style.border="1px solid #eaeaea";
   element.style.padding="8px 15px";
